9f1f8e33eb1aa8f06babc2758e6a1a30704e575a
mikasaBotz456 <108715025+mikasaBotz456@users.noreply.github.com>
2022-11-05 18:28:04 +0700

    update index.js

--- index.js
+++ index.js
@@ -214,7 +214,7 @@ XeonBotInc.sendMessage(anu.id, buttonMessage, {quoted:unicorndoc})
    │✑  ${xeontime} ${xeondate}
    └───────────────┈ ⳹`
 let buttons = [
-{buttonId: `wkwkwk`, buttonText: {displayText: 'Sayonara 🥀'}, type: 1}
+{buttonId: `wkwkwk`, buttonText: {displayText: 'Sayonara Sayangg🥀'}, type: 1}
 ]
 let buttonMessage = {
 document: fs.readFileSync('./XeonMedia/theme/cheems.xlsx'),
